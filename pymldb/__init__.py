8811c029e6003b9e43382a558f7e1b583d5772b7
Simon Lemieux <simon@datacratic.com>
2015-08-19 17:42:35 -0400

    add a few shortcuts to the Connection object

--- pymldb/__init__.py
+++ pymldb/__init__.py
@@ -5,6 +5,26 @@
 
 from pymldb import resource
 import pandas as pd
+from pprint import pprint
+
+
+class Color:
+    GREEN = '\033[32m'
+    RED = '\033[31m'
+    ORANGE = '\033[33m'
+    WHITE = '\033[37m'
+    RESET = '\033[0m'
+
+def color(text, color):
+    return getattr(Color, color.upper()) + str(text) + Color.RESET
+
+
+def unescapeSpecialParams(params):
+    params = dict(params)  # copy (not deep)
+    for p in params.keys():
+        if p.endswith('_'):
+            params[p[:-1]] = params.pop(p)
+    return params
 
 
 class Connection(object):
@@ -23,8 +43,54 @@ class Connection(object):
         else:
             return pd.DataFrame.from_records(resp_json, index="_rowName")
 
-    #def batframe(self, dataset_id):
-    #    return data.BatFrame(self.v1.datasets(dataset_id).uri)
+    # a few shortcuts
+    @property
+    def functions(self):
+        return self.v1.functions
+
+    @property
+    def procedures(self):
+        return self.v1.procedures
+
+    @property
+    def datasets(self):
+        return self.v1.datasets
+
+    # a few other shortcuts to create dataset/functions/procedures
+    def _create_thing(self, what, id, type_, **params):
+        params = unescapeSpecialParams(params)
+        thing = getattr(self, what)(id)
+        try:
+            print '\n', color('DELETE', 'red'), color(thing, 'white')
+            thing.delete()
+        except resource.ResourceError:
+            pass
+        jsonBlob = {'type': type_, 'params': params}
+        print '\n', color('PUT', 'orange'), color(thing, 'white')
+        # pprint(jsonBlob)
+        res = thing.put_json(jsonBlob)
+        # print '\nResult:'
+        pprint(res)
+        return thing
+
+    def create_dataset(self, id, type_=None, **params):
+        """ you can also pass a config dictionary """
+        if type(id) == dict:
+            return self.create_dataset(id['id'], id['type'], **id['params'])
+        elif type_ is None:
+            raise ValueError('you need a type_ for you dataset')
+        return self._create_thing('datasets', id, type_, **params)
+
+    def create_function(self, id, type_, **params):
+        return self._create_thing('functions', id, type_, **params)
+
+    def create_procedure(self, id, type_, **params):
+        if 'outputDataset' in params:
+            ds = self.datasets(params['outputDataset']['id'])
+            print '\n', color('DELETE', 'red'), color(ds, 'white')
+            ds.delete()
+        return self._create_thing('procedures', id, type_, **params)
+
 
 
 # IPython Magic system
